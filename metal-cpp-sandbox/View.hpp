d5f0dc4a91b637ad60aa6ef25dfa305499e7ddea
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-03-20 14:55:13 +0545

    add: metal-cpp example init (for xcode)

---
+++ metal-cpp-sandbox/View.hpp
@@ -0,0 +1,18 @@
+#ifndef View_hpp
+#define View_hpp
+
+#include <iostream>
+#include "Renderer.hpp"
+
+class MyMTKViewDelegate : public MTK::ViewDelegate
+{
+    public:
+        MyMTKViewDelegate( MTL::Device* pDevice );
+        virtual ~MyMTKViewDelegate() override;
+        virtual void drawInMTKView( MTK::View* pView ) override;
+
+    private:
+        Renderer* _pRenderer;
+};
+
+#endif
---
+++ metal-cpp-sandbox/main.cpp
@@ -0,0 +1,27 @@
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#include <Metal/Metal.hpp>
+#include <AppKit/AppKit.hpp>
+#include <MetalKit/MetalKit.hpp>
+
+
+#include "Application.hpp"
+
+
+int main( int argc, char* argv[] )
+{
+    NS::AutoreleasePool* pAutoreleasePool = NS::AutoreleasePool::alloc()->init();
+
+    MyAppDelegate del;
+
+    NS::Application* pSharedApplication = NS::Application::sharedApplication();
+    pSharedApplication->setDelegate( &del );
+    pSharedApplication->run();
+
+    pAutoreleasePool->release();
+
+    return 0;
+}
+
